bf3e965557dd19b66a254c9f58532a0afb0aeb66
Robert Jenkins <robertjenkins@Roberts-MacBook-Pro.local>
2019-03-03 17:55:57 -0800

    rj complete week 2

--- Week-2.js
+++ Week-2.js
@@ -1,40 +1,87 @@
 //-----------JavaScript Practical Assessment Week 2
 
-// 1. Write an arrow function that takes one argument of type array and returns the contents of that array in reverse order.
-
-var originalArray = [1, 2, 3, 4, 5, 6, 7]
-
-
-// 2. Given the object below, complete the console.log to find specific information:
-
-var bicycle = {
-	type: "Roadbike",
-	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
-	wheels: {
-		count: 2,
-		specs: ["road tires", "AX-7563", "80-115 PSI"],
-		brand: "Trek"
-	}
-}
-
-// Log the type of bicycle:
-console.log()
-
-// Log the bell:
-console.log()
-
-// Log the PSI:
-console.log()
-
+// // 1. Write an arrow function that takes one argument of type array and returns the contents of that array in reverse order.
+//
+// var originalArray = [1, 2, 3, 4, 5, 6, 7]
+//
+// reverseIt = (array) => {
+// 	return array.reverse()
+// }
+//
+// console.log(reverseIt(originalArray))
+// // 2. Given the object below, complete the console.log to find specific information:
+//
+// var bicycle = {
+// 	type: "Roadbike",
+// 	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
+// 	wheels: {
+// 		count: 2,
+// 		specs: ["road tires", "AX-7563", "80-115 PSI"],
+// 		brand: "Trek"
+// 	}
+// }
+//
+// // Log the type of bicycle:
+// console.log(bicycle.type)
+//
+// // Log the bell:
+// console.log(bicycle.gear[2])
+//
+// // Log the PSI:
+// console.log(biycle.specs[2])
+//
 
 // 3. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
 
-var ourString = "Hello Learn Students!"
+// var ourString = "Hello Learn Students!"
+//
+// const letterCounter = (string) => {
+// 	let newArray = []
+// 	let newWord = string.toLowerCase().split("")
+//
+// 	for(let i = 0; i < newWord.length; i++) {
+// 		if (newWord[i] === 'l'){
+// 			newArray.push(newWord[i])
+// 		}
+// 	}
+//
+// 	return newArray.length
+// }
+//
+// console.log(letterCounter(ourString))
 
 
-// 4. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// 4. Write a function called getFib that returns the first 10 numbers of the
+//Fibonacci sequence in an array. [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 
-//5. Write a function called oddChecker that takes in the fullArr and uses a for loop to return a new array of only odd numbers. Copy and paste your code and refactor using ES6 syntax.
+//
+// getFib = []
+// for(i = 0; getFib.length <= 9; i++) {
+//   getFib.length <= 1 ? getFib.push(1) : getFib.push(getFib[getFib.length - 1] + getFib[getFib.length - 2])
+// }
+//
+// console.log(getFib)
 
-var fullArr = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+//5. Write a function called oddChecker that takes in the fullArr and uses a
+//for loop to return a new array of only odd numbers.
+//Copy and paste your code and refactor using ES6 syntax.
+
+// var fullArr = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+//
+// var oddChecker = (array) => {
+// 	let finalArr =[]
+// 	for(let i = 0; i < array.length; i++){
+// 		if(typeof array[i] !== 'number'){
+// 		}else if(array[i] % 2 !== 0){
+// 			finalArr.push(array[i])
+// 		}
+// 	}
+// 	return finalArr
+// }
+// //with ES6 syntax
+// const checkOdds = (array) => {
+// 	return array.filter(x => x % 2 !== 0 && typeof x === 'number')
+// }
+// console.log(oddChecker(fullArr));
+// console.log(checkOdds(fullArr));
--- Week-2.md
+++ Week-2.md
@@ -3,37 +3,54 @@
 ### Without using Google answer the following:
 
 1. What is the difference between .map() and .filter()?
+The .filter() will return a smaller array.
 
 2. Why would you use object destructuring?
+It makes the code cleaner and easier to read.
 
 3. What is the difference between var, let, and const?
+"Var" tends to be used on a global scope and can be changed if need be. "let" tends to be used within the scope of a function. Const tends to be used when you dont want the variable to change throughout the process of adding additional code.  
 
 4. Why is testing important?
+Testing is important because it can help catch smaller bugs sooner. I think small bugs can turn into bigger bugs if not addressed from the get go. I also think it goes in hand with talking about what you want your application to do and once you figure that out, you can assess what needs to be tested.
 
 5. What is a higher order function?
+Functions that operate on other functions, either by taking them as arguments or by returning them.
 
 6. What is the difference between a class and an object?
+ A class is a vague definition on how things will be within my code. They are also seen as a blueprint. So, if I created a user class that takes in first name, last name and age. The object would be the filler information my class is looking to fill.
+
 
 7. What did you learn during the group project this week? Please include any additional feedback you may have.
-
+  I learned that I have to be more confident and speak up during group projects. Otherwise, I will slip into the void of silence and confusion.
 
 ### HTML/CSS Review questions: First, try to answer each question on your own then Google the answer to further your knowledge.
 
 1. How do you link a CSS file to your HTML page?
+ You create a separate file and save it as a css file. Then you link it to your html within the <head> portion of your file using a <link> tag.
 
 2. What is the difference between a div and a span?
+   Span is an inline approach and span would be used to span a larger block of code. Divs would be more useful when combined with flex box.
 
 3. What is a CSS class? When should you use an id instead of a class?
+    An ID would be used to style one thing on your page vs a class can be used to style multiple elements on a page.
 
 4. Name 4 semantic HTML tags.
+  <header>, <body>, <footer>  and <section>
 
 5. What are three options for creating responsive design?
+  We would add some buttons that have onclick responses. We could use flexbox. We could use REACT... Im not entirely sure about this answer.
 
 
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
 
 1. What is front end development? Can you identify any tools/skills that are uniquely required of front end developers?
+  Front end focuses more on the framework of your site and the styling of it. You would have to have a passion for html, css and have a focus for user interface.
 
 2. What is block scope in JavaScript?
+Block scope is the scope within curly brackets of a function.
+
+
 
 3. How would you explain the idea of "inheritance" in object oriented programming?
+In legend of zelda. A green rupee is worth 1. All other rupees inherit the same shape, sound, except the blue is has a different value of 5 and the red has a value of 10... ect.  This answer... may also be incorrect.  
